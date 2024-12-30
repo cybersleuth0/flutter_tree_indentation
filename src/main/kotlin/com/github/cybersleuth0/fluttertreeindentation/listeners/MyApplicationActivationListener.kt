acce5b44772533c3c1468a42adf806031293135b
GitHub Action <action@github.com>
2024-12-30 07:43:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/cybersleuth0/fluttertreeindentation/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/cybersleuth0/fluttertreeindentation/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cybersleuth0.fluttertreeindentation.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
